92afa03835c39f1ce0eebe7c382884a8e275f335
Max Fortun <max@maxf.net>
2019-08-16 19:31:23 -0400

    added

--- bin/solar_heater/off.sh
+++ bin/solar_heater/off.sh
@@ -5,6 +5,8 @@ BWD=$(dirname $SWD)
 
 . $BWD/pins
 
+$BWD/pump/off.sh
 $BWD/gpio.sh on $PIN_SOLAR_HEATER
 sleep 45
+$BWD/pump/on.sh
 
--- bin/solar_heater/on.sh
+++ bin/solar_heater/on.sh
@@ -5,7 +5,9 @@ BWD=$(dirname $SWD)
 
 . $BWD/pins
 
+$BWD/pump/off.sh
 $BWD/gpio.sh dir $PIN_SOLAR_HEATER out
 $BWD/gpio.sh off $PIN_SOLAR_HEATER
 sleep 45
+$BWD/pump/on.sh
 
