866e30e7e7f6198ea1e7f4384c6a7cbf7db04b68
Max Fortun <max@maxf.net>
2022-06-26 11:32:56 -0400

    Adding auto pump switch

--- bin/pump/on.sh
+++ bin/pump/on.sh
@@ -3,6 +3,13 @@
 SWD=$(dirname $(readlink -f $0))
 BWD=$(dirname $SWD)
 
+if [ "$1" = "auto" ]; then
+	auto=$(cat $SWD/auto.status)
+	if [ "$auto" != true ]; then
+		exit
+	fi
+fi
+
 . $BWD/pins
 
 $BWD/gpio.sh on $PIN_PUMP_PWR
